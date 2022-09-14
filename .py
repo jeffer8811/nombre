afe66b5012dd6575c0dd733358f2a6119fdebefe
jeffer8811 <112575785+jeffer8811@users.noreply.github.com>
2022-09-14 13:54:02 -0500

    Add files via upload

---
+++ .py
@@ -0,0 +1,13 @@
+lista =["pan", "leche", "azucar"]
+lista.append("maíz")
+lista.pop(-1)
+lista.sort()
+lista.reverse()
+lista.remove("pan")
+
+print(lista)
+
+
+
+tuple=("uno","dos","tre","cuatro","cuatro")
+tuple.count("uno","dos","tre","cuatro","cuatro")
---
+++ tupla.py
@@ -0,0 +1,31 @@
+
+
+from importlib import import_module
+
+
+waw={"gato":"cat","perro":"dog","cerdo":"pig"}
+print(waw["gato"])
+
+
+
+numero=int(input("inglesa tu edad"))
+if numero >=20:
+    print("es mayor de edad")
+elif numero<=0:
+    print("tu edad no puede ser menor a 0")  
+else:
+         print("eres menor de edad")
+
+print("inglesa tu edad")
+    
+
+
+print("comienzo")
+for w in[range (100)]:
+    print("Hola" , end="")
+print()
+print("fianl")
+
+    
+
+ 
